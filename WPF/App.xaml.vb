c39e6beba51cecb5015b63e81deab99f30774649
Raja Venkatesh <rrvenki@yahoo.com>
2019-06-14 14:47:34 +0530

    Update App.xaml.vb
    Change App.xaml.vb with Main subroutine

--- WPF/App.xaml.vb
+++ WPF/App.xaml.vb
@@ -1,3 +1,12 @@
-﻿Partial Class App
+imports System.Windows
 
+''' <summary>
+''' Interaction logic for App.xaml
+''' </summary>
+Partial Public Class App
+    'Inherits Application
+    Public Shared Sub Main()
+        Dim app As Application = New Application()
+        app.Run(New MainWindow())
+    End Sub
 End Class
